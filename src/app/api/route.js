5339bd663ec576e55264fe3e7dbed4f6a0a1945e
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-11-04 14:24:55 +0530

    wan

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,7 +1,9 @@
 import mysql from 'serverless-mysql';
 const db = mysql({
   config: {
-    host: '192.168.15.111',
+    // host: '180.151.132.231',
+    // host: '192.168.15.111',
+    host: '182.56.2.23',
     port: 3306,
     database: 'c1news',
     user: 'itmaint',
